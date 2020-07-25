f1d532fce2223a487379f9c1809281b5b13ddaaa
steven <stevensuarezcano@gmail.com>
2020-07-25 17:51:42 -0500

    initial commit, .env, routes and migration

---
+++ database/migrations/2020_07_25_224225_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Order;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('customer_name', 80);
+            $table->string('customer_email', 120);
+            $table->string('customer_mobile', 40);
+            $table->string('status', 20)->default(Order::STATUS_CREATED);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
