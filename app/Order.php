1cec9b7f3913d4c6de9f0689f3294bb7b82f1554
steven <stevensuarezcano@gmail.com>
2020-07-25 17:55:16 -0500

    models

--- app/Order.php
+++ app/Order.php
@@ -4,7 +4,39 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @method static create(array $validated)
+ * @method static get()
+ */
 class Order extends Model
 {
-    //
+    const STATUS_CREATED = "CREATED";
+    const STATUS_PAYED = "PAYED";
+    const STATUS_REJECTED = "REJECTED";
+
+    protected $fillable = [
+        'customer_name', 'customer_email', 'customer_mobile', 'status'
+    ];
+
+    protected $appends = [
+        'is_created',
+        'is_payed',
+        'is_rejected'
+    ];
+
+    public function getIsCreatedAttribute()
+    {
+        return $this->status == self::STATUS_CREATED;
+    }
+
+    public function getIsPayedAttribute()
+    {
+        return $this->status == self::STATUS_PAYED;
+    }
+
+    public function getIsRejectedAttribute()
+    {
+        return $this->status == self::STATUS_REJECTED;
+    }
+
 }
